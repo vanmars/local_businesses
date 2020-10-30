8d9affa4a619e89fa71cf8924a06c6c51e403e1e
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 16:34:15 -0700

    Add titleize and capitalize callbacks methods without content to restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -5,4 +5,10 @@ describe Restaurant do
   it { should validate_presence_of :kind }
   it { should validate_presence_of :description }
   it { should validate_presence_of :review }
+
+  it 'titleizes the name and kind of restaurant' do
+    restaurant = FactoryBot.create(:restaurant, name: 'test name', kind: 'test kind')
+    expect(restaurant.name).to eq('Test Name')
+    expect(restaurant.kind).to eq('Test Kind')
+  end
 end
