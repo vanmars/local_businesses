85eeb802a24bcbec49dde9ffd15fb2144e68c190
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 10:35:35 -0700

    Write and PASS restaurant#show request tests

---
+++ spec/requests/get_restaurant_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'Restaurant#show', :type => :request do
+
+  let!(:restaurants) { 
+    [
+      FactoryBot.create_list(:restaurant, 19),
+      FactoryBot.create(:restaurant, name: 'test_name', kind: 'test_kind', description: 'test_description', review: 'test_review', id: 20)
+    ]
+  }
+
+  before { get '/restaurants/20' }
+    
+  # Test Header
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+  # Test Body
+  it 'returns correct data' do
+    expect(JSON.parse(response.body)['name']).to eq('test_name')
+    expect(JSON.parse(response.body)['kind']).to eq('test_kind')
+    expect(JSON.parse(response.body)['description']).to eq('test_description')
+    expect(JSON.parse(response.body)['review']).to eq('test_review')
+  end
+
+end
