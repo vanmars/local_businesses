3d843ff41c6e61fecfd9f1966651aba94511e0f4
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 09:14:24 -0700

    Add file for seeding 50 restaurants and 50 shops

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed = seed.generate_restaurants
+    seed = seed.generate_shops
+  end
+
+  def generate_restaurants
+    50.times do |i|
+      restaurant = Restaurant.create!(
+        name: Faker::Restaurant.name,
+        type: Faker::Restaurant.type,
+        description: Faker::Restaurant.description,
+        review: Faker::Restaurant.review
+      )
+    end
+    puts "50 restaurants successfully generated"
+  end
+
+  def generate_shops
+    50.times do |i|
+      shop = Shop.create!(
+        name: Faker::Company.name,
+        industry: Faker::Company.industry,
+        logo_url: Faker::Company.logo,
+        slogan: Faker::Company.catch_phrase
+      )
+    end
+    puts "50 shops successfully generated"
+  end
+end
+
+Seed.begin
