dfda816a162951e4239ebf8e7acc17dca0b4ceab
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 16:43:29 -0700

    Write and FAIL test for restaurant capitalize callback method

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -11,4 +11,10 @@ describe Restaurant do
     expect(restaurant.name).to eq('Test Name')
     expect(restaurant.kind).to eq('Test Kind')
   end
+
+  it 'capitalizes the description and review of restaurant' do
+    restaurant = FactoryBot.create(:restaurant, description: 'test description', review: 'test review')
+    expect(restaurant.description).to eq('Test description')
+    expect(restaurant.review).to eq('Test review')
+  end
 end
