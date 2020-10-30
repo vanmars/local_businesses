38e758586ed730797043d39be61946fca7d85049
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 11:45:05 -0700

    Add routes for custom endpoints search and random

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :restaurants
   resources :shops
+
+  # Custom Endpoints
+  get 'restaurants/random', to: 'restaurants#random'
+  get 'restaurants/search', to: 'restaurants#search'
+
+  get 'shops/random', to: 'shops#random'
+  get 'shops/search', to: 'shops#search'
 end
