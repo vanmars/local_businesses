5f85ee92a7d9d9d1acf3fc399cf1510d74a18f2e
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 11:08:59 -0700

    Write and PASS request test for restaurant#destroy

---
+++ spec/requests/delete_restaurant_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Restaurant#destroy', :type => :request do
+let!(:restaurants) { 
+  [
+    FactoryBot.create_list(:restaurant, 19),
+    FactoryBot.create(:restaurant, name: 'test_name', kind: 'test_kind', description: 'test_description', review: 'test_review', id: 20)
+  ]
+}
+
+  before do
+     delete '/restaurants/20'
+  end
+
+  # Test Header
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+  # Test Body
+  it 'returns successful delete message' do
+    expect(JSON.parse(response.body).values[0]).to eq("Restaurant successfully deleted.")
+  end
+  
+end
