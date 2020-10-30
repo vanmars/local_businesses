e71332c27b8e845bb075663f8de439f5b7f7bba7
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 11:40:04 -0700

    Write and PASS tests for shop pagination

--- spec/requests/get_shops_spec.rb
+++ spec/requests/get_shops_spec.rb
@@ -1,17 +1,36 @@
 require 'rails_helper'
 
 describe 'Shop#index', :type => :request do
-  let!(:shops) { FactoryBot.create_list(:shop, 20) }
+  let!(:shops) { FactoryBot.create_list(:shop, 100) }
 
-  before { get '/shops' }
+  # WITHOUT PAGINATION
+  context 'without page parameters' do
+    before { get '/shops' }
 
-  # Test Heeader
-  it 'returns status code 200' do
-    expect(response).to have_http_status(:success)
+    # Test Heeader
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:success)
+    end
+
+    # Test Body
+    it 'returns all shops' do
+      expect(JSON.parse(response.body).size).to eq(100)
+    end
   end
 
-  # Test Body
-  it 'returns all shops' do
-    expect(JSON.parse(response.body).size).to eq(20)
+  # WITH PAGINATION
+  context 'wit page parameters' do
+    before { get '/shops', params: { page: 1 } }
+
+    # Test Heeader
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:success)
+    end
+
+    # Test Body
+    it 'returns restaurants on page 1' do
+      expect(JSON.parse(response.body).size).to eq(25)
+    end
   end
+
 end
