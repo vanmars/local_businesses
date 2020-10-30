a3492d8312dbbe113d1fc002b48bedc20962346e
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 09:34:05 -0700

    Add methods skeleton for shops controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,33 @@
 class ShopsController < ApplicationController
 
+  def index
+    
+  end
 
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+private
+
+  def shop_params
+
+  end
+
+  def set_shop
+    
+  end
   
 end
