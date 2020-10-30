7e0f41f3bb055d96a18222a390947d2004e262f3
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 10:41:47 -0700

    Write and PASS restaurant#show test for 404 id not found error

--- spec/requests/get_restaurant_spec.rb
+++ spec/requests/get_restaurant_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+#################
+# SUCCESSFUL CALL
+#################
 describe 'Restaurant#show', :type => :request do
 
   let!(:restaurants) { 
@@ -22,6 +25,26 @@ describe 'Restaurant#show', :type => :request do
     expect(JSON.parse(response.body)['kind']).to eq('test_kind')
     expect(JSON.parse(response.body)['description']).to eq('test_description')
     expect(JSON.parse(response.body)['review']).to eq('test_review')
+  end
+end
+
+################################
+# UNSUCCESSFUL CALL - Invalid ID
+################################
+describe 'Restaurant#show', :type => :request do
+
+  let!(:restaurants) { FactoryBot.create_list(:restaurant, 20) }
+
+  before { get '/restaurants/100' }
+    
+  # Test Header
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:not_found)
+  end
+
+  # Test Body
+  it 'returns correct data' do
+    expect(JSON.parse(response.body)['message']).to eq("Couldn't find Restaurant with 'id'=100")
   end
 
 end
