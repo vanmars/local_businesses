fdaf5dd52b05134fe19ceaeb8e7b840c7a19b0c5
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 09:27:27 -0700

    Build out basic CRUD functionality for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,37 @@
 class RestaurantsController < ApplicationController
 
+  def index
+    @restaurants = Restaurant.all
+    json_response(@restaurants)
+  end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    json_response(@restaurant)
+  end
+
+  def create
+    @restaurant = Restaurant.create!(restaurant_params)
+    json_response(@restaurant, :created)
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update!(restaurant_params)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+  end
+
+private
+  def restaurant_params
+    params.permit(:name, :kind, :description, :review)
+  end
+
+  def json_response(object, status=:ok)
+    render json: object, status: status
+  end
   
 end
